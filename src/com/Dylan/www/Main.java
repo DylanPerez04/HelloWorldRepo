789edc0b8841e38cb594cc91dcde3151a87e1d53
Dylan Perez <dylanperez.h04@gmail.com>
2024-08-18 16:51:48 -0700

    Hello World set up with author comments

---
+++ src/com/Dylan/www/Main.java
@@ -0,0 +1,12 @@
+package com.Dylan.www;
+
+/**
+ * @author Dylan Perez-Hernandez
+ * @version 1.0.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
